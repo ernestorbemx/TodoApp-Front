a3f4c81e3bfe2b249c26dc650735544ab5b3cb7e
ernestorbemx <ernesto.ramirez@encora.com>
2025-04-03 23:09:02 -0600

    Update filter component props

--- src/components/TodoFilters.tsx
+++ src/components/TodoFilters.tsx
@@ -4,16 +4,17 @@ import { Priority } from "../http/todo";
 import { Input } from "@heroui/input";
 import { TodoFilter } from "../types";
 import { useState } from "react";
+import { Button } from "@heroui/button";
 
-export const states: { label: string, value: boolean | null }[] = [
-    { label: "All", value: null },
+export const states: { label: string, value: boolean | '' }[] = [
+    { label: "All", value: '' },
     { label: "Done", value: true },
     { label: "Undone", value: false },
 ]
 
 
-export const priorities: { label: string, value: Priority | null }[] = [
-    { label: "All", value: null },
+export const priorities: { label: string, value: Priority | '' }[] = [
+    { label: "All", value: '' },
     { label: "High", value: "HIGH" },
     { label: "Medium", value: "MEDIUM" },
     { label: "Low", value: "LOW" },
@@ -21,16 +22,19 @@ export const priorities: { label: string, value: Priority | null }[] = [
 
 
 export interface TodoFiltersProps {
-    onChange: (filter: TodoFilter) => unknown
+    onChange: (filter: TodoFilter) => unknown,
+    searching: boolean
 }
 
-export function TodoFilters() {
+export function TodoFilters({ onChange, searching }: TodoFiltersProps) {
     const [filter, setFilter] = useState<TodoFilter>({})
 
     return <Card className="p-4">
         <div className="flex gap-8 items-center ">
             <label className="w-16 text-left flex-shrink-0">Text</label>
-            <Input label="Text search" placeholder="Write a text the todo must contain"></Input>
+            <Input label="Text search" placeholder="Write a text the todo must contain" onValueChange={(text) => {
+                setFilter(f => ({ ...f, text: text }))
+            }}></Input>
         </div>
         <div className="flex gap-8 items-center  ">
             <label className="w-16 text-left flex-shrink-0">Priority</label>
@@ -40,6 +44,9 @@ export function TodoFilters() {
                 items={priorities}
                 label="Priority filter"
                 placeholder="Select a priority to filter by"
+                onSelectionChange={(key) => {
+                    setFilter(f => ({ ...f, priority: key.currentKey as Priority }))
+                }}
             >
                 {(priority: typeof priorities[0]) => <SelectItem key={priority.value}>{priority.label}</SelectItem>}
             </Select>
@@ -49,12 +56,16 @@ export function TodoFilters() {
             <Select
                 size="sm"
                 className="max-w-xs"
-                items={priorities}
+                items={states}
                 label="State filter"
                 placeholder="Select a state to filter by"
+                onSelectionChange={(key) => {
+                    setFilter(f => ({ ...f, done: key.currentKey as unknown as boolean }))
+                }}
             >
-                {(priority: typeof priorities[0]) => <SelectItem key={priority.value}>{priority.label}</SelectItem>}
+                {(state: typeof states[0]) => <SelectItem key={String(state.value)}>{state.label}</SelectItem>}
             </Select>
         </div>
+        <Button onPress={() => onChange(filter)} isLoading={searching}>Search</Button>
     </Card >
 }
