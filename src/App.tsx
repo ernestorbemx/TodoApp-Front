10e7aaf9ba416f84d25eacac08b4f57dc41c5daf
ernestorbemx <ernesto.ramirez@encora.com>
2025-04-03 23:13:01 -0600

    Add HeroUI ToastProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 import { HeroUIProvider } from '@heroui/system'
 import { TodoView } from './views/TodoView'
-
+import {ToastProvider} from "@heroui/toast";
 function App() {
   return (
     <HeroUIProvider>
+      <ToastProvider />
       <TodoView></TodoView>
     </HeroUIProvider>
   )
